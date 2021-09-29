7325bf519985cc11add4e241269dea633496ca7f
Alex Dumitru <adum6@student.monash.edu>
2021-09-29 23:36:25 +1000

    Update MainTest.java
    Resolved JUnit test setup issues that prevented some methods from having their class variables instantiated correctly

--- StringInterpreterAlgorithm/src/MainTest.java
+++ StringInterpreterAlgorithm/src/MainTest.java
@@ -1,17 +1,18 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    @BeforeEach
+    public void setUp() {
         Main.fetchSynonyms();
+        Main.allTargets = Main.processTargets(Main.uiElements);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void cleanElementsTest() {
         String String0 = null; // Breaks cleanElements, needs null handling
         String String1 = ""; // Empty string
@@ -35,7 +36,7 @@ class MainTest {
 
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void processTargetsTest() {
         String[] appNames = new String[]{"PlayStore", "Google","Google Plus","Facebook","Youtube", "Music", "Amazon Store",
                 "Alexa Play", "Music downloader", "Smart Launcher", "The New York Times", "Maps", "Duolingo: learn a language for free!"};
@@ -60,7 +61,7 @@ class MainTest {
 
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void isSingularPluralTests() {
         assertTrue(Main.isSingularPlural("maps","map"));
         assertTrue(Main.isSingularPlural("google maps","google map"));
@@ -74,9 +75,10 @@ class MainTest {
 
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void isTriggerWordTests() {
-        Main.fetchSynonyms();
+
+
         assertEquals("maps",(Main.isTriggerWord("maps")));  // Not a valid Trigger word, should return the original string
         assertEquals("",(Main.isTriggerWord("")));      // Empty string, should return empty string
         //System.out.println(isTriggerWord(null));  // null input, should handle error
@@ -104,7 +106,7 @@ class MainTest {
         assertEquals("ENTER!",Main.isTriggerWord("ENTER!"));    // Invalid action should return original string
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void isActionTests() {
         // Tests for isAction()
         //Main.fetchSynonyms();
@@ -120,7 +122,7 @@ class MainTest {
         assertTrue(Main.isAction("Enter"));      //valid action, should return TRUE
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getMaxTargetStringTests() {
         assertEquals("smart launcher we google about the proposal", Main.getMaxTargetString("launch smart launcher we google about the proposal",6)); //valid, should return "smart launcher we google about the proposal"
         assertEquals("youtube music", Main.getMaxTargetString("hello open youtube music",10));  //valid, should return "youtube music"
@@ -131,39 +133,40 @@ class MainTest {
 
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getActionTriggersTests() {
-        assertEquals("{OPEN={1=[0, 6]}}", Main.getActionTriggers("launch smart launcher we google about the proposal")); //valid, should return {OPEN={1=[0, 6]}}
-        assertEquals("{CLICK={1=[10, 15], 2=[33, 36]}}", Main.getActionTriggers("ok google click youtube and then tap home")); //valid uses of CLICK including synonym handling, should return {CLICK={1=[10, 15], 2=[33, 36]}}
-        assertEquals("{SCROLL={1=[12, 18]}, CLICK={1=[28, 31]}}", Main.getActionTriggers("hey google, scroll down and tap chrome")); //valid uses of SCROLL and CLICk, should return {SCROLL={1=[12, 18]}, CLICK={1=[28, 31]}}
-        assertEquals("{CLICK={1=[11, 14], 2=[15, 20], 3=[21, 26], 4=[27, 32]}}", Main.getActionTriggers("TESTINGTAP tap Click touch press ebay")); //valid uses of SCROLL and CLICk, should return {CLICK={1=[11, 14], 2=[15, 20], 3=[21, 26], 4=[27, 32]}}
-        assertEquals("{SCROLL={1=[6, 11], 2=[12, 16]}}", Main.getActionTriggers("SWIPE swipe move Go")); //valid uses of SCROLL SYNONYMS, should return {SCROLL={1=[6, 11], 2=[12, 16]}}
-        assertEquals("{OPEN={1=[0, 6], 2=[15, 20], 3=[21, 25]}}", Main.getActionTriggers("launch eXEcute start OPEN settings"));    //valid uses of OPEN SYNONYMS, {OPEN={1=[0, 6], 2=[15, 20], 3=[21, 25]}}
-        assertEquals("{ENTER={1=[0, 5], 2=[6, 10], 3=[11, 17], 4=[18, 22]}}", Main.getActionTriggers("ENTER type search fill username"));       //valid uses of ENTER SYNONYMS,{ENTER={1=[0, 5], 2=[6, 10], 3=[11, 17], 4=[18, 22]}}
-        assertEquals("{}", Main.getActionTriggers("hello this message is empty")); //valid, although no Action words used, should return {}
-        assertEquals("{}", Main.getActionTriggers("")); //valid, empty string, should return {}
+        assertEquals("{OPEN={1=[0, 6]}}", Main.getActionTriggers("launch smart launcher we google about the proposal").toString()); //valid, should return {OPEN={1=[0, 6]}}
+        assertEquals("{CLICK={1=[10, 15], 2=[33, 36]}}", Main.getActionTriggers("ok google click youtube and then tap home").toString()); //valid uses of CLICK including synonym handling, should return {CLICK={1=[10, 15], 2=[33, 36]}}
+        assertEquals("{SCROLL={1=[12, 18]}, CLICK={1=[28, 31]}}", Main.getActionTriggers("hey google, scroll down and tap chrome").toString()); //valid uses of SCROLL and CLICk, should return {SCROLL={1=[12, 18]}, CLICK={1=[28, 31]}}
+        assertEquals("{CLICK={1=[11, 14], 2=[15, 20], 3=[21, 26], 4=[27, 32]}}", Main.getActionTriggers("TESTINGTAP tap Click touch press ebay").toString()); //valid uses of SCROLL and CLICk, should return {CLICK={1=[11, 14], 2=[15, 20], 3=[21, 26], 4=[27, 32]}}
+        assertEquals("{SCROLL={1=[0, 5], 2=[6, 11], 3=[12, 16], 4=[17, 19]}}", Main.getActionTriggers("SWIPE swipe move Go home").toString()); //valid uses of SCROLL SYNONYMS, should return {SCROLL={1=[0, 5], 2=[6, 11], 3=[12, 16], 4=[17, 19]}}
+        assertEquals("{OPEN={1=[0, 6], 2=[7, 14], 3=[15, 20], 4=[21, 25]}}", Main.getActionTriggers("launch eXEcute start OPEN settings").toString());    //valid uses of OPEN SYNONYMS, {OPEN={1=[0, 6], 2=[7, 14], 3=[15, 20], 4=[21, 25]}}
+        assertEquals("{ENTER={1=[0, 5], 2=[6, 10], 3=[11, 17], 4=[18, 22]}}", Main.getActionTriggers("ENTER type search fill username").toString());       //valid uses of ENTER SYNONYMS,{ENTER={1=[0, 5], 2=[6, 10], 3=[11, 17], 4=[18, 22]}}
+        assertEquals("{}", Main.getActionTriggers("hello this message is empty").toString()); //valid, although no Action words used, should return {}
+        assertEquals("{}", Main.getActionTriggers("").toString()); //valid, empty string, should return {}
 
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getActionTargetMapTests() {
+
         Main.getActionTriggers("ok google click youtube and then home"); //valid uses of CLICK and target (youtube), should return {CLICK=[Youtube]}
-        assertEquals("{CLICK=[Youtube]}", Main.getActionTargetMap());
+        assertEquals("{CLICK=[Youtube]}", Main.getActionTargetMap().toString());
 
         Main.getActionTriggers("hey google launch facebook"); //valid uses of OPEN synonym (launch) and target (facebook), should return {OPEN=[Facebook]}
-        assertEquals("{OPEN=[Facebook]}", Main.getActionTargetMap());
+        assertEquals("{OPEN=[Facebook]}", Main.getActionTargetMap().toString());
 
         Main.getActionTriggers("hey google"); //valid, no actions or targets ,should return empty set {}
-        assertEquals("{}", Main.getActionTargetMap());
+        assertEquals("{}", Main.getActionTargetMap().toString());
 
         Main.getActionTriggers("scroll down please"); // valid uses of SCROLL, no target selected as it is not an app. Returns {SCROLL=[]}
-        assertEquals("{SCROLL=[]}", Main.getActionTargetMap());
+        assertEquals("{SCROLL=[]}", Main.getActionTargetMap().toString());
 
         Main.getActionTriggers("hello search youtube"); //valid uses of ENTER synonym (search) and target (youtube), should return {ENTER=[Youtube]}
-        assertEquals("{ENTER=[Youtube]}", Main.getActionTargetMap());
+        assertEquals("{ENTER=[Youtube]}", Main.getActionTargetMap().toString());
 
         Main.getActionTriggers(""); //Empty action list, should return empty dataset {}
-        assertEquals("{OPEN=[Facebook]}", Main.getActionTargetMap());
+        assertEquals("{}", Main.getActionTargetMap().toString());
 
     }
 }
